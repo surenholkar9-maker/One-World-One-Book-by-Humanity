f43480d8cade2617d4269d599fe68122876c52cf
surenholkar9-maker <surenholkar9@gmail.com>
2026-04-02 18:58:47 +0530

    Initialize App component with basic structure

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+function App() {
+  return (
+    <div className="App">
+      <h1>Spark App</h1>
+      {/* Add your recreated UI/logic here */}
+    </div>
+  );
+}
+export default App;
